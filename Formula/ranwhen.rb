5b1fccb5e425940cfcc421994ebd3482e2cb2449
Daniel Gustaw <gustaw.daniel@gmail.com>
2026-09-09 21:44:52 +0200

    build: bump packaging to 0.2.1

--- Formula/ranwhen.rb
+++ Formula/ranwhen.rb
@@ -1,8 +1,8 @@
 class Ranwhen < Formula
   desc "Visualize when your system was running (with native macOS screen/power tracking)"
   homepage "https://github.com/gustawdaniel/ranwhen"
-  url "https://github.com/gustawdaniel/ranwhen/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "7ca51a3e9c5538178a451d80f1886cbe3a9a753e1a3feef940c1aa4e9feb1312"
+  url "https://github.com/gustawdaniel/ranwhen/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "ff09b46d0fcdd83a19eca2404d0c62f89203f81069a5955a552c22581690f895"
   license "GPL-3.0-or-later"
   head "https://github.com/gustawdaniel/ranwhen.git", branch: "master"
 
